96afcb50c5712336694fbb3a5c72abd84f589a4c
Nathalie Krieg <nathalie.krieg@webapps.ch>
2023-11-04 10:46:13 +0100

    bootstrap implemented landing-page Component created

--- dark-patterns/src/app/app-routing.module.ts
+++ dark-patterns/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LandingPageComponent} from "./modules/components/landing-page/landing-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingPageComponent},
+  {path: '**', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
