42fcd94f6fd0b104bc422ab3b1cc6cd9f3c29669
Nathan Richard <nathanrichard@bluewin.ch>
2023-11-04 16:01:56 +0100

    feat: navbar component

--- dark-patterns/src/app/app.module.ts
+++ dark-patterns/src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LandingPageComponent } from './modules/components/landing-page/landing-page.component';
+import { NavbarComponent } from './modules/components/navbar/navbar.component';
+import {NgOptimizedImage} from "@angular/common";
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    NgOptimizedImage
   ],
   providers: [],
   bootstrap: [AppComponent]
